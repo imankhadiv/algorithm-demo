37023866280e1caa6cef71b6b3b233468c006a15
iman <iman.khadiv@gmail.com>
2017-11-14 20:02:02 -0800

    added more numeric algorithm

--- src/main/java/com/elrast/Numeric.java
+++ src/main/java/com/elrast/Numeric.java
@@ -28,7 +28,7 @@ public class Numeric {
 
     public boolean isPrimeNumberSqrt(int number) {
 
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i < Math.sqrt(number); i++) {
             for (int j = 1; j < Math.sqrt(number); j++) {
                 if (j * i == number) {
                     return false;
@@ -38,13 +38,70 @@ public class Numeric {
         return true;
     }
 
-//    public boolean isPrimeNumber2(int number) {
-//
-//        if(number <= 3){
-//            return true;
-//        }
-//        if(number % 2 == 0 || number % 3 == 0){
-//            return false;
-//        }
-//    }
+    public boolean isPrimeNumberSqrt2(int number) {
+
+        if (number % 2 == 0 || number % 3 == 0) {
+            return false;
+        }
+        for (int i = 3; i < Math.sqrt(number); i += 2) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String convertToBase(int number, int base) {
+        String result = "";
+        while (number >= base) {
+
+            result = number % base + result;
+            number /= base;
+        }
+        return number + result;
+    }
+
+    public String convertToBase2(int number, int base) {
+        if (number < base) {
+            return number + "";
+        }
+        return convertToBase(number / base, base) + number % base;
+    }
+
+    public int findTheGreatestCommonDenominator(int num1, int num2) {
+
+        int smallerNumber = num1 >= num2 ? num2 : num1;
+        int result = num1 >= num2 ? num1 % num2 : num2 % num1;
+        if (result == 0) return num1 >= num2 ? num2 : num1;
+
+        int sqrtVal = ((Double) (Math.floor(Math.sqrt(smallerNumber)))).intValue();
+
+        for (int i = sqrtVal; i > 1; i--) {
+            if (num1 % i == 0) {
+                int div = num1 / i;
+                if (num2 % i == 0) {
+                    return i;
+                } else if (num2 % div == 0) {
+                    return div;
+                }
+            }
+        }
+        return 1;
+    }
+
+    public int gcd(int num1, int num2) {
+
+        if (num2 == 0) {
+            return num1;
+        }
+        return gcd(num2, num1 % num2);
+    }
+
+    public int factorial(int number) {
+
+        if (number <= 1) {
+            return 1;
+        }
+        return number * factorial(--number);
+    }
 }
